7db602d5dd2382e6aef866813298e8cd00c34a84
imomintariq <85645222+imomintariq@users.noreply.github.com>
2022-04-19 19:35:53 +0500

    Initial commit

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,34 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
+    private static Stage stg;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stg = stage;
+        String fxmlPath = "WelcomeScreen/WelcomeScreen.fxml";
+        URL url = getClass().getClassLoader().getResource(fxmlPath);
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        Scene scene = new Scene(fxmlLoader.load(), 630, 450);
+        stage.setTitle("HMS");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException{
+        URL url = getClass().getClassLoader().getResource(fxml);
+        Parent pane = FXMLLoader.load(url);
+        stg.getScene().setRoot(pane);
+
+    }
+}
